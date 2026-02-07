86b96871093af8707dd874c26411a634e084797f
Kuwon Sebastian Na <laggu91@gmail.com>
2026-02-07 14:57:08 +0900

    chore: go releaser (#9)

--- .agent/skills/korean_plan/SKILL.md
+++
@@ -1,6 +0,0 @@
----
-name: korean plan
-description: show plan in korean
----
-
-plan 을 유저한테 보여줄떄 한국어로 보여줘
---
+++ cmd/version.go
@@ -0,0 +1,31 @@
+/*
+Copyright © 2026 laggu
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// Version information (set by goreleaser via ldflags)
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("git-volume %s\n", version)
+		fmt.Printf("  commit: %s\n", commit)
+		fmt.Printf("  built:  %s\n", date)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
